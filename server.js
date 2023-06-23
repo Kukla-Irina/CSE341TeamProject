cec5a6edec1b60e35497679965bdfc0675c89328
caclark17 <cla20055@byui.edu>
2023-06-23 17:11:07 -0600

    added more code

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hey there');
-});
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.listen(process.env.port || 3000);
-console.log('Web server is listening at port ' + (process.env.port || 3000));
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
