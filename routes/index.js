a412136cff26943b86c9390647593f999c537b00
caclark17 <cla20055@byui.edu>
2023-06-24 13:44:04 -0600

    added swagger

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/cakes', require('./cakes'))
-router.use('/clients', require('./clients'))
-router.use('/orders', require('./orders'))
-router.use('/sweets', require('./sweets'))
+router.use('/', require('./swagger'));
+
+router.use('/cakes', require('./cakes'));
+router.use('/clients', require('./clients'));
+router.use('/orders', require('./orders'));
+router.use('/sweets', require('./sweets'));
 
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'Bakery API',
+        description: 'Bakery app that allows customers to place orders for cakes and sweets'
+    },
+    host: 'localhost:3000',
+    schemes: ['http']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+//generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
